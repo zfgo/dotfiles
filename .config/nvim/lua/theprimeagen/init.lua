acac2105067054afa71a539b0defce09458c3154
Zane Globus-O'Harra <zanegloh@gmail.com>
2024-10-09 21:58:49 -0400

    changes

--- .config/nvim/lua/theprimeagen/init.lua
+++ .config/nvim/lua/theprimeagen/init.lua
@@ -1,5 +1,17 @@
 require("theprimeagen.set")
 require("theprimeagen.remap")
+require("theprimeagen.lazy_init")
+
+-- DO.not
+-- DO NOT INCLUDE THIS
+
+-- If i want to keep doing lsp debugging
+-- function restart_htmx_lsp()
+--     require("lsp-debug-tools").restart({ expected = {}, name = "htmx-lsp", cmd = { "htmx-lsp", "--level", "DEBUG" }, root_dir = vim.loop.cwd(), });
+-- end
+
+-- DO NOT INCLUDE THIS
+-- DO.not
 
 local augroup = vim.api.nvim_create_augroup
 local ThePrimeagenGroup = augroup('ThePrimeagen', {})
@@ -10,6 +22,12 @@ local yank_group = augroup('HighlightYank', {})
 function R(name)
     require("plenary.reload").reload_module(name)
 end
+
+vim.filetype.add({
+    extension = {
+        templ = 'templ',
+    }
+})
 
 autocmd('TextYankPost', {
     group = yank_group,
@@ -26,6 +44,23 @@ autocmd({"BufWritePre"}, {
     group = ThePrimeagenGroup,
     pattern = "*",
     command = [[%s/\s\+$//e]],
+})
+
+autocmd('LspAttach', {
+    group = ThePrimeagenGroup,
+    callback = function(e)
+        local opts = { buffer = e.buf }
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    end
 })
 
 vim.g.netrw_browse_split = 0
